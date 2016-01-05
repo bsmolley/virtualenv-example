30918ba0ef338e9a7c814ffe05eaed9d99b272bc
bsmolley <bsmolley@ualberta.ca>
2016-01-05 09:18:03 -0700

    Update

--- virtualenv-test.py
+++ virtualenv-test.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+# virtualenv <NameOfEnv>
+# cd <NameOfEnv>
+
 import requests
 
 print requests.__version__
+
+response = requests.get("http://google.ca")
+print response.status_code
+
+print response.text
